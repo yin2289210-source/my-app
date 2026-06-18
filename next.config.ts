6c7f05eb0b039bb840a7a510f089d0efd9c085d7
yin2289210-source <yin2289210@gmail.com>
2026-06-18 17:00:41 +0800

    2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',   // 开启静态导出模式
-  images: {
-    unoptimized: true, // 静态导出必须关闭图片优化
-  },
-};
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
